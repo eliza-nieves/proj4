8c0f1bb1039a1170ef360c04b42c338468ce72e3
Eliza Nieves <snowfell00@gmail.com>
2020-04-01 21:35:44 -0400

    Finished primary stage of UI

--- src/projectFiles/Controller.java
+++ src/projectFiles/Controller.java
@@ -1,4 +1,18 @@
 package projectFiles;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+
 public class Controller {
+
+    @FXML
+    private ListView names;
+
+    public void initialize(){
+        names.getItems().addAll("Beef", "Cheese", "Chicken","Green Pepper", "Ham",
+                "Mushroom", "Onion", "Pepperoni", "Pineapple", "Sausage");
+        names.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+    }
+
 }
--- src/projectFiles/Main.java
+++ src/projectFiles/Main.java
@@ -1,20 +1,29 @@
 package projectFiles;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.SelectionMode;
 import javafx.stage.Stage;
+import javafx.scene.control.ListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 600));
+        primaryStage.setTitle("Pizza Order");
+        primaryStage.setScene(new Scene(root, 450, 650));
         primaryStage.show();
         primaryStage.setResizable(false);
+
+
+
+
     }
 
 
