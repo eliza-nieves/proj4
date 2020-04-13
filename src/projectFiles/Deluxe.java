bf89a31aa920a6c34a96137d7280056b048e0e37
mohammad <mohammad@viveks-mbp.hsd1.nj.comcast.net>
2020-04-13 13:50:46 -0400

    Complete PrimaryStage and PrimaryController

---
+++ src/projectFiles/Deluxe.java
@@ -0,0 +1,34 @@
+package projectFiles;
+
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class Deluxe extends Pizza{
+	//members: String style
+	// String size
+	// ArrayList<String> toppings
+	private int basePrice = 14;
+
+	public Deluxe(String style, String size) {
+		super(style, size);
+		this.toppings = new ArrayList<String>(5);
+		Collections.addAll(this.toppings, "Sausage","Pepperoni","Grenn Pepper","Onion","Mushroom");
+	}
+
+	@Override
+	public int pizzaPrice() {
+		int res = basePrice;
+		if(this.style.equals("Medium")) {
+			res += mediumIncrement;
+		}else if(this.style.equals("Large")) {
+			res += largeIncrement;
+		}
+		return res;
+	}
+	
+	public String toString() {
+		String res = super.toString();
+		res = res + " Price: $" + this.pizzaPrice();
+		return res;
+	}
+}
